6a8172a125baf4674d03b12942d5574b337a54d3
xionghui <xionghui@lerjin.com>
2024-10-11 23:35:12 +0800

    1

--- Assets/Script/BG.cs
+++ Assets/Script/BG.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class BG : MonoBehaviour
 {
+    private Vector2 movement;
+    public Vector2 sleep;
+    private Material _material;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _material = GetComponent<Renderer>().material;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement += sleep*Time.deltaTime;
+        Debug.Log(movement);
+        _material.mainTextureOffset = movement;
+
+
     }
 }
